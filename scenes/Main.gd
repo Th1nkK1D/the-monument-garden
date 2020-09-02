e854cd15f3c7caf0e74d7531e85e2d1b5505c3d1
Th1nkK1D <witheep@gmail.com>
2020-09-02 20:46:42 +0700

    Remove unused rotation mechanic

--- scenes/Main.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-
-
-func _on_ArrowClockWise_is_clicked():
-	$World.rotate_one_forth_anticlockwise()
-
-func _on_ArrowAntiClockWise_is_clicked():
-	$World.rotate_one_forth_clockwise()
